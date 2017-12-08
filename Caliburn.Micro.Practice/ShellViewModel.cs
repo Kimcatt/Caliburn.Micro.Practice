d588a724db6c448fc9ad99009425d1f316e31005
Kimcatt <kimcode@foxmail.com>
2017-12-08 17:34:09 +0800

    Caliburn.Micro Practice
    Caliburn.Micro Practice

---
+++ Caliburn.Micro.Practice/ShellViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Caliburn.Micro.Practice
+{
+    [Export(typeof(IShell))]
+    public class ShellViewModel : Caliburn.Micro.PropertyChangedBase, IShell
+    {
+        string name = "你好，请开始Caliburn.Micro之旅！";
+
+        private System.Timers.Timer clockTimer;
+
+        DateTime now = DateTime.Now;
+
+        public DateTime Now
+        {
+            get
+            {
+                return now;
+            }
+            set
+            {
+                now = value;
+                NotifyOfPropertyChange(() => Now);
+            }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                NotifyOfPropertyChange(() => Name);
+                //NotifyOfPropertyChange(() => CanWelcome);
+            }
+        }
+
+
+        public ShellViewModel()
+        {
+            clockTimer = new System.Timers.Timer();
+            clockTimer.Interval = TimeSpan.FromSeconds(1).TotalMilliseconds;
+            clockTimer.Elapsed += (sender, e) =>
+            {
+                Now = DateTime.Now;
+            };
+            clockTimer.Start();
+        }
+
+
+        //public bool CanWelcome
+        //{
+        //    get { return !string.IsNullOrWhiteSpace(Name); }
+        //}
+
+        public void Welcome()
+        {
+            MessageBox.Show(string.Format("提示： {0}", Name)); //Don't do this in real life :)
+        }
+    }
+}
---
+++ Kimcatt.Mvvm/ValueConverter/DateTimerConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Kimcatt.Mvvm.ValueConverter
+{
+    public class DateTimerConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime)
+            {
+                return (string)((value as DateTime?)?.ToString("yyyy-MM-dd HH:mm:ss"));
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string)
+            {
+                return DateTime.Parse(value as string);
+            }
+            return null;
+        }
+    }
+}
